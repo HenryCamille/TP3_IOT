3bfdf1c9ad6b63ba2af71327470b5b30273a8ebd
HenryCamille <camille.henry614@gmail.com>
2021-12-10 12:25:39 +0100

    enregistrement des valeures

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+import { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import InstantHeartRate from "./Composants/InstHeartRate";
 
 function App() {
+  const [measurements, setMeasurements] = useState([50]);
+  useEffect(() =>{
+    setInterval(()=>{
+      
+      setMeasurements((prev) => {
+     return [...prev, Math.floor(Math.random() * (200 - 40) + 40)]
+      }); 
+    }, 5000);
+  }, [])
+
+  const measurement = measurements [ measurements.length - 1];
+  console.log(measurement);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Une application pour les BPM.</p>
-        <InstantHeartRate />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <InstantHeartRate data ={measurement}/>
       </header>
     </div>
   );
--- src/Composants/InstHeartRate.js
+++ src/Composants/InstHeartRate.js
@@ -1,25 +1,30 @@
-function InstantHeartRate() {
 
-    var val = Math.floor(Math.random() * (200 - 40) + 40);
+
+
+function InstantHeartRate(props) {
+
+    var {data} = props; //Met les valeurs de l'App.js dans le composant.
+    console.log(data); 
 
     var classe = "color";
 
-    if (val > 120){
+    if (data > 120){
 
         classe="danger";
-        val = val + " BPM : ATTENTION ! ";
+        data = data + " BPM : ATTENTION ! ";
     }
-    else if (val < 50){
+    else if (data < 50){
         classe="danger";
-        val = val + " BPM : ATTENTION ! ";
+        data = data + " BPM : ATTENTION ! ";
     }
 
     else{
         classe="ok";
-        val = val + " BPM : Normal ";
+        data = data + " BPM : Normal ";
     }
     return (
-        <div class={classe}>Rythme cardiaque : {val} </div>
+        <div class={classe}>Rythme cardiaque : {data} 
+        </div>
       )
     }
     
